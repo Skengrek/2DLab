3996c967365b064ad3fb1c6aa9662f039ab4f3db
Armand Massé <armand.masse@annalect.com>
2024-10-31 16:28:14 +0100

    WIP

--- 2dlab/src/components/GameOfLife/MainContainer1.vue
+++ 2dlab/src/components/GameOfLife/MainContainer1.vue
@@ -1,14 +1,15 @@
 <script setup>
-    import { Application, Ticker, Container, Sprite, Texture} from 'pixi.js';
+    import { Application, Graphics, Sprite} from 'pixi.js';
 </script>
 
 <template>
     <div id="pixi-container"></div>
     <div>{{ time }}ms</div>
-    <button @click="regeneratGrid">Regenerate</button>
-    <button @click="processTick">One Tick</button>
-    <button @click="appTicker.start">Start Ticker</button>
-    <button @click="appTicker.stop">Stop Ticker</button>
+    <button @click="randomizeGrid">Regenerate</button>
+    <!-- <button @click="processTick">One Tick</button> -->
+    <button @click="start">Start Ticker</button>
+    <button @click="stop">Stop Ticker</button>
+    <button @click="reset">Reset</button>
 </template>
 
 <script>
@@ -17,99 +18,128 @@ export default {
     data() {
         return {
             app: new Application(),
-            container: new Container(),
-            appTicker: Ticker.shared,
-            width: 1000,
-            height: 1000,
-            particuleWidth: 10,
-            particuleHeight: 10,
-            time_per_frame:50,
-            tick: 0,
-            grid: [],
-            newGrid: [],
+            // Container
+            width: 512,
+            height: 512,
+            gridWidth: 256,
+            gridHeight: 256,
+            // Data
+            data: new Uint32Array(),
             sprites: [],
             isSpriteCreated: false,
             time: 0,
+            // Graphics
+            liveColor: 0x00ff00,
+            deadColor: 0x000000,
+            runnning: false,
         }
     },
     async mounted(){
-        await this.app.init({ width: this.width, height: this.height})
-        this.app.stage.addChild(this.container)
-        document.getElementById('pixi-container').appendChild(this.app.canvas);
-        this.startUp()
-        this.regeneratGrid()
+        await this.app.init({
+            width: this.width,
+            height: this.height
+        })
+        this.init()
+        document.getElementById('pixi-container').appendChild(this.app.canvas)
+        this.data = new Uint32Array((this.gridWidth * this.gridHeight) / 32)
+        this.randomizeGrid()
     },
     methods: {
-        startUp(){
-            this.grid = []
-            this.newGrid = []
-            this.sprites = []
-            for (let x=0; x<this.width/this.particuleWidth;x++){
-                this.grid[x] = []
-                this.newGrid[x] = []
-                this.sprites[x] = []
-                for (let y=0; y<this.height/this.particuleHeight; y++){
-                    this.grid[x][y] = {value:0}
-                    this.newGrid[x][y] = {value:0}
-                    const rect = new Sprite(Texture.WHITE)
-                    rect.width=this.particuleWidth
-                    rect.height=this.particuleHeight
-                    rect.x = x*this.particuleWidth
-                    rect.y = y*this.particuleHeight
-                    this.sprites[x][y] = rect
-                    this.container.addChild(rect)
-                }
-            }
-
-        },
-        regeneratGrid(){
-            this.initValues()
-            this.initTicker()
-        },
-        initValues(){
-            for (let x in this.grid){
-                for (let y in this.grid[x]){
-                    let value = Math.random() > 0.7
-                    this.grid[x][y]={value:value}
-                    this.sprites[x][y].renderable = value
+        init(){
+            for (let y = 0; y < this.gridHeight; y++) {
+                for (let x = 0; x < this.gridWidth; x++) {
+                    const sprite = new Sprite(this.deadTexture);
+                    sprite.x = x * this.cellSize;
+                    sprite.y = y * this.cellSize;
+                    this.app.stage.addChild(sprite);
+                    this.sprites.push(sprite);
                 }
             }
         },
-        initTicker(){
-            this.appTicker.autoStart = false
-            this.appTicker.add(this.processTick)
+        cellSize(){
+            return this.width / this.gridWidth
         },
-        processTick(){
-            const start = performance.now();
-
-            for (let x = 0; x < this.grid.length; x++) {
-                for (let y = 0; y < this.grid[x].length; y++) {
-                    const sumAlive = this.countAliveNeighbor(x, y);
-                    const isAlive = this.grid[x][y].value;
-                    const newValue = (isAlive && (sumAlive === 2 || sumAlive === 3)) || (!isAlive && sumAlive === 3);
-                    this.newGrid[x][y] = { value: newValue };
-                    this.sprites[x][y].renderable = newValue;
+        gridSize(){
+            return this.gridWidth * this.gridHeight
+        },
+        createCellTexture (color) {
+            const cellGraphics = new Graphics();
+            cellGraphics.rect(0, 0, this.cellSize, this.cellSize);
+            cellGraphics.fill(color);
+            return this.app.renderer.generateTexture(cellGraphics);
+        },
+        getCell(x, y){
+            if (x < 0 || y < 0 || x >= this.gridWidth || y >= this.gridHeight) return 0;
+            const index = y * this.gridWidth + x;
+            const dataIndex = Math.floor(index / 32);
+            const bitPosition = index % 32;
+            return (this.data[dataIndex] & (1 << bitPosition)) !== 0 ? 1 : 0;
+        },
+        setCell(x, y, state) {
+            const index = y * this.gridWidth + x;
+            const dataIndex = Math.floor(index / 32);
+            const bitPosition = index % 32;
+            if (state) {
+                this.data[dataIndex] |= 1 << bitPosition;
+            } else {
+                this.data[dataIndex] &= ~(1 << bitPosition);
+            }
+        },
+        randomizeGrid() {
+            for (let y = 0; y < this.gridHeight; y++) {
+                for (let x = 0; x < this.gridWidth; x++) {
+                    const state = Math.random() > 0.5 ? 1 : 0;
+                    this.setCell(x, y, state);
+                    this.sprites[y * this.gridWidth + x].texture = state ? this.liveTexture : this.deadTexture;
                 }
             }
-
-            [this.grid, this.newGrid] = [this.newGrid, this.grid]; // Swap references
-            this.time = performance.now() - start;
         },
-        countAliveNeighbor(x, y){
-            const maxX = this.grid.length;
-            const maxY = this.grid[0].length;
-            let nbAliveNeighbor = 0;
+        updateGrid () {
+            const newGrid = new Uint32Array(this.data.length);
+            for (let y = 0; y < this.gridHeight; y++) {
+                for (let x = 0; x < this.gridWidth; x++) {
+                    const index = y * this.gridWidth + x;
+                    const currentState = this.getCell(x, y);
 
-            for (let dx = -1; dx <= 1; dx++) {
-                for (let dy = -1; dy <= 1; dy++) {
-                    if (dx === 0 && dy === 0) continue;
-                    const nx = (x + dx + maxX) % maxX;
-                    const ny = (y + dy + maxY) % maxY;
-                    if (this.grid[nx][ny].value) nbAliveNeighbor++;
+                    let liveNeighbors = 0;
+                    for (let i = -1; i <= 1; i++) {
+                        for (let j = -1; j <= 1; j++) {
+                            if (i === 0 && j === 0) continue;
+                            liveNeighbors += this.getCell(x + i, y + j);
+                        }
+                    }
+
+                    const nextState = (currentState === 1 && (liveNeighbors === 2 || liveNeighbors === 3)) || (currentState === 0 && liveNeighbors === 3) ? 1 : 0;
+
+                    const dataIndex = Math.floor(index / 32);
+                    const bitPosition = index % 32;
+                    if (nextState) {
+                        newGrid[dataIndex] |= 1 << bitPosition;
+                    } else {
+                        newGrid[dataIndex] &= ~(1 << bitPosition);
+                    }
+
+                    if (nextState !== currentState) {
+                        this.sprites[index].texture = nextState ? this.liveTexture : this.deadTexture;
+                    }
                 }
             }
+            this.data.set(newGrid);
+        },
+        start() {
+            this.running = true;
+            this.app.ticker.add(this.updateGrid);
+        },
 
-            return nbAliveNeighbor;
+        stop() {
+            this.running = false;
+            this.app.ticker.remove(this.updateGrid);
+        },
+
+        reset() {
+            this.stop()
+            for (let i = 0; i < this.data.length; i++) this.data[i] = 0
+            this.sprites.forEach(sprite => (sprite.texture = this.deadTexture))
         },
     }
 }
